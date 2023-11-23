0a9d0a2b649b297d3ee0b550ebe3a5caa292a2f7
TGW <mc-casper@hotmail.dk>
2023-11-23 14:20:33 +0100

    Permits the program to fetch pods to assign to the client

--- GameLobby/Assign_Server.cs
+++ GameLobby/Assign_Server.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using k8s;
 using k8s.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace GameLobby
 {
@@ -21,21 +23,85 @@ namespace GameLobby
             if (client == null)
                 client = MakeKubernetesClient();
 
-            // Namespace where your GameServer service is deployed
+            string GameServerIP;
+
+            // GameServer's namespace
             var namespaceName = "default";
 
-            // Service name of the GameServer
-            var gameServerServiceName = "gameserver-service";
+            List<ServerIP> podListEven = new();
+            List<ServerIP> podListUneven = new();
+            var labelSelector = "game-server-pod";
+            var pods = client.ListNamespacedPod(namespaceName, labelSelector: labelSelector);
+            foreach(var pod in pods.Items)
+            {
+                var podIP = pod.Status.PodIP;
 
-            // Get the service
-            var service = client.ReadNamespacedService(gameServerServiceName, namespaceName);
+                var query = QueryGameServerStatus(podIP);
+                query.GetAwaiter().GetResult();
+                GameServerStatus? status = query.Result;
+                if(status != null)
+                {
+                    if (IsOdd(status.ActivePlayers))
+                        podListUneven.Add(new(status.ActivePlayers, pod.Status.PodIP));
+                    else
+                        podListEven.Add(new(status.ActivePlayers, pod.Status.PodIP));
+                }
+            }
+            List<ServerIP> sortedList;
+            if(podListUneven.Count > 0)
+                sortedList = podListUneven.OrderBy(pod => pod.ActivePlayers).ToList();
+            else
+                sortedList = podListEven.OrderBy(pod => pod.ActivePlayers).ToList();
 
-            // Extract GameServer IP
-            var gameServerIP = service.Spec.ClusterIP;
+            GameServerIP = sortedList.First().IP;
 
-            Console.WriteLine($"GameServer IP: {gameServerIP}");
+            Console.WriteLine($"GameServer IP: {GameServerIP}");
 
-            return gameServerIP;
+            return GameServerIP;
+        }
+
+        private static bool IsOdd (int value)
+        {
+            return value % 2 != 0;
+        }
+
+        public class ServerIP
+        {
+            public int ActivePlayers { get; set; }
+            public string IP { get; set; }
+
+            public ServerIP(int players, string ip)
+            {
+                ActivePlayers = players;
+                IP = ip;
+            }
+        }
+
+        static async Task<GameServerStatus?> QueryGameServerStatus(string IP)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync($"http://{IP}/status");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var status = await response.Content.ReadFromJsonAsync<GameServerStatus>();
+                        return status;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error querying game server status: {ex.Message}");
+            }
+            return null;
+        }
+
+        public class GameServerStatus
+        {
+            public int ActivePlayers { get; set; }
         }
 
         static private Kubernetes MakeKubernetesClient()
@@ -43,7 +109,6 @@ namespace GameLobby
             // Load Kubernetes configuration from the default location or a specified file
             var config = KubernetesClientConfiguration.BuildDefaultConfig();
 
-            // Create Kubernetes client
             var client = new Kubernetes(config);
 
             return client;
